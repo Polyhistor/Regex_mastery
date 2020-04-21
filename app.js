b399b05287696c3dc51e967bf73e46181982ed64
Polyhistor <poouya.ataei.7@gmail.com>
2020-04-21 21:44:45 +1200

    a sweet way to replace first and last name

--- app.js
+++ app.js
@@ -22,3 +22,25 @@ let reg2 = /\w+(?=\.com)/g
 
 let arr2 = data2.match(reg2)
 
+
+// Iterate through the data provided. Use a regular expression to store the names 
+// in a new array but change the order of the name 
+// so first name is listed first and last name is listed last
+
+let data3 = ["Jensen, Dale", "Smith, Andrea", "Jorgensen, Micheal", "Vasefi, Annika", "Lopez, Monica", "Crcokett, Steven"]
+
+let reg3 = /(\w+) (\w+)/;
+let newData = data3.map(name => {
+    console.log(name.replace(/(\w+), (\w+)/, "$2 $1"))
+})
+
+// a generic password regex
+
+let password="N2RN3/>zvd"
+
+let theLength = /^.{8,32}$/
+let upper = /^[A-Z]/
+let lower = /^[a-z]/
+let numbers = /[0-9]/
+let special = /[^0-9A-Za-z]/
+
